995860cd46d73c17002c36ed69867077db79293a
Glen <house5775@gmail.com>
2025-03-15 20:29:06 +0800

    set bootstrap ENV

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-
+import { useEffect} from "react"
+import axios from "axios"
 
 
 function App() {
-
+  useEffect(()=>{
+    (async()=>{
+      try {
+        const res = await axios.get('https://randomuser.me/api/')
+        console.log(res)
+      } catch (error) {
+        console.log(error)
+      }
+    })()
+    },[])
 
   return (
     <>
+    <h1 className="fs-7 fs-lg-2">hello React</h1>
     </>
   )
 }
