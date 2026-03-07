24ebff6070f1cc0f0291745baf584ba86aa7f901
Glen <house5775@gmail.com>
2026-03-07 13:40:55 +0800

    Updates

--- assets/cartSelectors-CS9rDrmx.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./reselect-CiXCCDx7.js";const r=e=>e.cart,l=t([r],e=>e.cartList),y=t([r],e=>e.paymentMethod),g=t([r],e=>e.isCartLoading),u=t([l],e=>{const s={shortBoard:500,longBoard:500,midLength:500};return e.carts&&e.carts.length>0?e.carts.reduce((c,o)=>{const a=o.product.category,n=s[a]*o.qty||0;return c+n},0):0});export{g as a,u as b,y as c,l as s};
